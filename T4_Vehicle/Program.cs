e9e16484dc26113a02725c8a3219a0a56a168945
rikukal <h8851@student.jamk.fi>
2016-01-31 14:28:57 +0200

    lisätty T4

---
+++ T4_Vehicle/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4_Vehicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle();
+            vehicle.Name = " Kaara";
+            vehicle.Speed = 0;
+            vehicle.Tyres = 4;
+            vehicle.PrintData(); 
+
+            // metodit, eli olion toiminnot	(methods)
+            // Jarrutus ja Kaasutus
+
+            /*
+            public void Kaasuta()
+            {
+                Speed += 20;
+            }
+
+            public void Kaasuta_kovaa()
+            {
+                Speed += 50;
+            }
+
+            public void Jarruta()
+            {
+                Speed -= 20;
+            }
+
+            public void Jarruta_kovaa()
+            {
+                Speed -= 50;
+            }
+
+            public void Lisaa_rengas()
+            {
+                Tyres += 1;
+            }
+
+            public void Ota_rengas()
+            {
+                Tyres -= 1;
+            }
+        */
+
+            // palauttaa kaikki Vehiclen ominaisuudet yhtenä merkkijonona
+            Console.WriteLine(vehicle.ToString());
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Vehicle/Vehicle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4_Vehicle
+{
+    class Vehicle
+    {   //ominaisuudet (properties) HUOM pääohjelmassa ISOILLA ALKUKIRJAIMILLA
+        public string Name { get; set; }
+        public double Speed { get; set; }
+        public int Tyres { get; set; }
+
+        // default constructor
+
+        //parametric constructor
+
+        // methods 
+
+        public void PrintData()
+        {
+            Console.WriteLine("Vehicle data");
+            Console.WriteLine("- Name{0}", Name);
+            Console.WriteLine("- Speed {0}", Speed);
+            Console.WriteLine("- Tyres {0}", Tyres);
+        }
+    }
+
+        /* 
+        vehicle.Kaasuta();
+        vehicle.Kaasuta_kovaa();
+        vehicle.Jarruta();
+        vehicle.Jarruta_kovaa();
+        vehicle.Lisaa_rengas();
+        vehicle.Ota_rengas(); */
+
+        public override string ToString(Vehicle)
+        {
+
+            return "Name: " + Name + " Speed: " + Speed + " Tyres: " + Tyres;
+        }
+}
