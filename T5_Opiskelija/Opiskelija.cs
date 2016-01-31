34806042730c91d26fd42906b5802b0f44afb668
rikukal <h8851@student.jamk.fi>
2016-01-31 17:42:19 +0200

    yritys T5

--- T5_Opiskelija/Opiskelija.cs
+++ T5_Opiskelija/Opiskelija.cs
@@ -7,6 +7,22 @@ using System.Threading.Tasks;
 namespace T5_Opiskelija
 {
     class Opiskelija
-    {
+    {   //ominaisuudet (properties) Isolla Alkukirjaimella!
+        public string Nimi { get; set; }
+        public int Ika { get; set; }
+        public int Opintopisteet { get; set; }
+        public string Opiskeluala { get; set; }
+        public bool Insinoori { get; set; }
+
+        // default constructor ( ominaisuuden oletus-alustus)
+
+        //parametric constructor
+
+        // olion toimminnot eli metodit (methods) 
+        public void PrintData()
+        {
+            Console.WriteLine("Tiedot opiskelijoista: Nimi: {0} , Ika: {1} , Opintopisteet: {2} , Opiskeluala {3} , Onko Insinoori: {4} ", Nimi, Ika, Opintopisteet, Opiskeluala, Insinoori);
+        }
+
     }
 }
--- T5_Opiskelija/Program.cs
+++ T5_Opiskelija/Program.cs
@@ -10,6 +10,47 @@ namespace T5_Opiskelija
     {
         static void Main(string[] args)
         {
+            Opiskelija opiskelijat [] = new Opiskelija [5];
+            opiskelijat [0] = new Opiskelija();
+
+            /*  opiskelijat[0].Nimi = "Riku";
+              opiskelijat[1].Nimi = "Kalle";
+              opiskelijat[2].Nimi = "Iina";
+              opiskelijat[3].Nimi = "Susanna";
+              opiskelijat[4].Nimi = "Kerttu"; */
+
+            /*    opiskelijat[0].Ika = 20 ;
+                opiskelijat[1].Ika = 23 ;
+                opiskelijat[2].Ika = 19 ;
+                opiskelijat[3].Ika = 27 ;
+                opiskelijat[4].Ika = 57 ; */
+
+            /*  opiskelijat[0].Opintopisteet = 60;
+              opiskelijat[1].Opintopisteet = 23;
+              opiskelijat[2].Opintopisteet = 100;
+              opiskelijat[3].Opintopisteet = 100;
+              opiskelijat[4].Opintopisteet = 120; */
+
+            /*  opiskelijat[0].Opiskeluala = "tieto ja viestinta-tekniikka";
+                opiskelijat[1].Opiskeluala = "hyvinvointiteknologia";
+                opiskelijat[2].Opiskeluala = "sairaanhoitaja";
+                opiskelijat[3].Opiskeluala = "energia ja ymparisto-tekniikka";
+                opiskelijat[4].Opiskeluala = "fysioterapeutti"; */
+
+
+            /* opiskelijat[0].Insinoori = true;
+             opiskelijat[1].Insinoori = true;
+             opiskelijat[2].Insinoori = false;
+             opiskelijat[3].Insinoori = true;
+             opiskelijat[4].Insinoori = false; */
+
+            /*for (int i = 0; i < opiskelijat.Length; i++) Console.WriteLine(opiskelijat[i]);*/
+
+            /*foreach ( Opiskelija opiskelija in opiskelijat)
+            { student.PrintData();
+            }*/
+
+
         }
     }
 }
