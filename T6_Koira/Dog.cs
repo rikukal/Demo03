22bc575adde48955233c31bedd04be3bfbc8187a
rikukal <h8851@student.jamk.fi>
2016-02-02 21:39:41 +0200

    T6-muutokset

--- T6_Koira/Dog.cs
+++ T6_Koira/Dog.cs
@@ -11,23 +11,27 @@ namespace T6_Koira
         public string Name { get; set; }
         public string Breed { get; set; }   //rotu
         public int Age { get; set; }
-        public int Volume { get; set; } //Bark_more or Bark_less
+        //public enum Volume { get; set; } //Bark_more or Calm asteet: silent, quiet, bit noisy, loud, boisterous ENUM?
         public int Tailspeed { get; set; } // Swinging tail , Stop , swing less
         public bool Drooling { get; set;}   //kuolaako vai ei? - oletus että kuolaisi  
 
         // default constructor
 
         //parametric constructor
-
+       
         // methods 
         public void PrintData()
         {
             Console.WriteLine(" Dog info:");
             Console.WriteLine(" Name: {0}", Name);
             Console.WriteLine(" Age {0}", Age);
-            Console.WriteLine(" Volume: {0}",);
-            Console.WriteLine({0})      //Tähän jäätiin!
-            Console.WriteLine({0})
+            //Console.WriteLine(" Volume: {0}",Volume);
+            Console.WriteLine("{0}", Tailspeed);
+            Console.WriteLine("{0}", Drooling);
+        }
+        public override string ToString()
+        {
+            return "Name: " + Name + " Age: " + Age + /*" Volume: "+ Volume + */" Tailspeed: " + Tailspeed + "Is drooling:" + Drooling;
         }
     }
 }
--- T6_Koira/Program.cs
+++ T6_Koira/Program.cs
@@ -10,6 +10,26 @@ namespace T6_Koira
     {
         static void Main(string[] args)
         {
+            Dog dog = new Dog();
+            dog.Name = "Musti";
+            dog.Age = 3;
+            //dog.Volume = "quiet";
+            dog.Tailspeed = 3;
+            dog.Drooling = true;
+            dog.PrintData();
         }
+
+        /* Volume volume = Volume Quiet
+            enum {
+            Silent,
+            Quiet,
+            Noisy,
+            Loud,
+            Boisterous
+        } */
+
+        Console.WriteLine(dog.ToString());
+
+            Console.ReadLine();
     }
 }
